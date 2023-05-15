bec3a0712d0e01c02c31c72d22a519ca77f410b6
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-15 17:17:33 +0000

    final edit

---
+++ go_app/main.go
@@ -0,0 +1,45 @@
+// Created by: Dominic M.
+// Created on: April 2023
+//
+// This program divides two numbers.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var numberA int
+	var numberB int
+	var counter int
+
+	// input
+	fmt.Println("This program divides two numbers.")
+	fmt.Println()
+	fmt.Print("Enter your first number: ")
+	fmt.Println()
+	fmt.Scanln(&numberA)
+	fmt.Println()
+	fmt.Print("Enter your second number: ")
+	fmt.Println()
+	fmt.Scanln(&numberB)
+	fmt.Println()
+
+	// process
+	var remainder int = numberA
+	
+	for remainder >= numberB {
+		remainder -= numberB
+		counter++
+	}
+
+	if remainder > 0 {
+		fmt.Print(numberA, " ÷ ", numberB, " = ", counter, " with a remainder of ", remainder, ".")
+	} else {
+		fmt.Print(numberA, " ÷ ", numberB, " = ", counter)
+	}
+
+	fmt.Println()
+	fmt.Print("\nDone.")
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Dominic M. All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dominic M.
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+/**
+ * This function uses a selection component from https://github.com/CreativeIT/getmdl-select
+ */
+
+function myButtonClicked() {
+
+  // input
+  const numberA = parseInt(document.getElementById('number-a').value)
+  const numberB = parseInt(document.getElementById('number-b').value)
+  let remainder = numberA
+  let output = 0
+
+  // process
+
+  while (remainder >= numberB) {
+    remainder -= numberB
+    output++
+  }
+  if (remainder > 0) {
+    document.getElementById('answer').innerHTML = numberA + " ÷ " + numberB + " = " + output + " with a remainder of " + remainder + "."
+  } else {
+    document.getElementById('answer').innerHTML = numberA + " ÷ " + numberB + " = " + output + "."
+  }
+}
